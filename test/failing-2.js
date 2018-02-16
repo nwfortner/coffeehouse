725588fae42511c4d38cde3404924d84cb2edb65
Michael Schoonmaker <schoon@testdouble.com>
2018-02-16 14:57:56 -0800

    Fix race condition in start/end events.

--- test/failing-2.js
+++ test/failing-2.js
@@ -1,3 +1,6 @@
+const crypto = require('crypto')
+const key = crypto.pbkdf2Sync('secret', 'salt', 1000000, 64, 'sha512')
+
 describe('Failing tests 2: Error Boogaloo', function () {
   it('should fail', function () {
     function stackTest() {
